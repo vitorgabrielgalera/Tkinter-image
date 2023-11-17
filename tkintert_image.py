1b5bd1190eaa87fe340dc90e5fd507c533892ce6
Vitor Gabriel <vitor.ggabrielgalera@gmail.com>
2023-11-17 20:52:41 -0300

    mais imagens salvas(sem que apareçam todas)

--- tkintert_image.py
+++ tkintert_image.py
@@ -1,18 +1,30 @@
 #importo as bibliotecas
 from tkinter import *
 from PIL import ImageTk, Image
+import os
 
 #abro uma janela
 root = Tk()
 
-#abro a imagem e a salvo em uma variável
-imagem = Image.open("capivara.png")
+#pega um arquivo e faz uma lista com o que tem tudo no arquivo
+arquivos = os.listdir("imagens")
 
-#defino a imagem com foto/imagem
-imagem_tk = ImageTk.PhotoImage(imagem)
+#armazena as imagens
+imagens = []
+
+#percorre cada arquivo dentro de arquivos
+for arquivo in arquivos:
+    #abre imagem
+    img = Image.open("imagens/" + arquivo)
+    #adiciona a imagem na lista
+    imagens.append(ImageTk.PhotoImage(img))
+
+#exibe arquivos em um label
+#arquivos_label = Label(root, text=arquivos)
+#arquivos_label.pack()
 
 #defino os parâmetros da imagem
-imagem_Label = Label(root, image=imagem_tk)
+imagem_Label = Label(root, image=imagens[0])
 
 #abro a imagem na janela
 imagem_Label.pack()
