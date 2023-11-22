621b79875366b47f81fb77c87822ecf8994e0e46
Vitor Gabriel <vitor.ggabrielgalera@gmail.com>
2023-11-22 20:36:25 -0300

    Criação dos botões next, prev e sair

--- tkintert_image.py
+++ tkintert_image.py
@@ -23,11 +23,65 @@ for arquivo in arquivos:
 #arquivos_label = Label(root, text=arquivos)
 #arquivos_label.pack()
 
+#variavel de controle de imagem
+imagem_atual = 0
+
 #defino os parâmetros da imagem
-imagem_Label = Label(root, image=imagens[0])
+imagem_Label = Label(root, image=imagens[imagem_atual])
 
 #abro a imagem na janela
-imagem_Label.pack()
+imagem_Label.grid(row=0, column=0, columnspan=3)
+
+#defino as funções de avançae e voltar
+def prev_image():
+    
+    #transformo as variáveis em globa
+    global imagem_atual
+    global imagem_Label
+    global imagens
+
+    #se a variavel for a primeira ela vai para a ultima
+    if imagem_atual == 0:
+        imagem_atual = len(imagens) - 1
+    else:
+        imagem_atual -= 1
+
+    #apaga a imagem
+    imagem_Label.grid_forget()
+
+    #exibe a nova imagem
+    imagem_Label = Label(root, image=imagens[imagem_atual])
+    imagem_Label.grid(row=0, column=0, columnspan=3)
+
+def next_image():
+    #transformo as variáveis em globa
+    global imagem_atual
+    global imagem_Label
+    global imagens
+
+    #se a variavel for a primeira ela vai para a ultima
+    if imagem_atual == len(imagens) - 1:
+        imagem_atual = 0
+    else:
+        imagem_atual += 1
+
+    #apaga a imagem
+    imagem_Label.grid_forget()
+
+    #exibe a nova imagem
+    imagem_Label = Label(root, image=imagens[imagem_atual])
+    imagem_Label.grid(row=0, column=0, columnspan=3)
+
+#crio e posiciono os botões
+prev = Button(root,text="prev",command=prev_image)
+prev.grid(row=1, column=0)
+
+#o comando é um já pronto do Tk
+sair = Button(root,text="sair", command=root.quit)
+sair.grid(row=1, column=1)
+
+next = Button(root,text="next",command=next_image)
+next.grid(row=1, column=2)
 
 #rodo a janela
 root.mainloop()
